2c9488a72f461c71fb47599faec3cd73b3490eb3
Sabbir Hasan <sabbir.gh@gmail.com>
2020-04-12 02:44:13 +0600

    some section update

--- home-page.php
+++ home-page.php
@@ -88,65 +88,36 @@ get_header();?>
 		            <div class="row d-flex justify-content-center">
 		                <div class="menu-content  col-lg-7">
 		                    <div class="title text-center">
-		                        <h1 class="mb-10">My Offered Services</h1>
-		                        <p>At about this time of year, some months after New Year’s resolutions have been made and kept, or made and neglected.</p>
+							<?php if ( get_field('service_title') ) : ?>
+								<h1 class="mb-10"><?php echo get_field('service_title'); ?></h1>
+								<?php endif; ?>
+							
+		                        <?php if ( get_field('service_text_area') ) : ?>
+									<p><?php echo get_field('service_text_area'); ?></p>	
+								<?php endif; ?> 
 		                    </div>
 		                </div>
 		            </div>						
 					<div class="row">
-						<div class="col-lg-4 col-md-6">
+					<?php if ( have_rows('services') ) : ?>
+					
+						<?php while( have_rows('services') ) : the_row(); ?>
+							
+							<div class="col-lg-4 col-md-6">
 							<div class="single-services">
 								<span class="lnr lnr-pie-chart"></span>
-								<a href="#"><h4>Web Design</h4></a>
+								<a href="<?php the_sub_field('service_item_icon_url'); ?>"><h4><?php the_sub_field('service_item_title'); ?></h4></a>
 								<p>
-									“It is not because things are difficult that we do not dare; it is because we do not dare that they are difficult.”
+								<?php the_sub_field('service_item_description'); ?>
 								</p>
 							</div>
-						</div>
-						<div class="col-lg-4 col-md-6">
-							<div class="single-services">
-								<span class="lnr lnr-laptop-phone"></span>
-								<a href="#"><h4>Web Development</h4></a>
-								<p>
-									If you are an entrepreneur, you know that your success cannot depend on the opinions of others. Like the wind, opinions.
-								</p>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-6">
-							<div class="single-services">
-								<span class="lnr lnr-camera"></span>
-								<a href="#"><h4>Photography</h4></a>
-								<p>
-									Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills.
-								</p>
-							</div>	
-						</div>
-						<div class="col-lg-4 col-md-6">
-							<div class="single-services">
-								<span class="lnr lnr-picture"></span>
-								<a href="#"><h4>Clipping Path</h4></a>
-								<p>
-									Hypnosis quit smoking methods maintain caused quite a stir in the medical world over the last two decades. There is a lot of argument.
-								</p>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-6">
-							<div class="single-services">
-								<span class="lnr lnr-tablet"></span>
-								<a href="#"><h4>Apps Interface</h4></a>
-								<p>
-									Do you sometimes have the feeling that you’re running into the same obstacles over and over again? Many of my conflicts.
-								</p>
-							</div>
-						</div>
-						<div class="col-lg-4 col-md-6">
-							<div class="single-services">
-								<span class="lnr lnr-rocket"></span>
-								<a href="#"><h4>Graphic Design</h4></a>
-								<p>
-									You’ve heard the expression, “Just believe it and it will come.” Well, technically, that is true, however, ‘believing’ is not just thinking that.
-								</p>
-							</div>				
+						</div>	
+					
+						<?php endwhile; ?>
+					
+					<?php endif; ?>
+					
+								
 						</div>														
 					</div>
 				</div>	
@@ -157,22 +128,20 @@ get_header();?>
 			<section class="facts-area section-gap" id="facts-area">
 				<div class="container">				
 					<div class="row">
+					<?php if ( have_rows('counter') ) : ?>
+					
+						<?php while( have_rows('counter') ) : the_row(); ?>
 						<div class="col-lg-3 col-md-6 single-fact">
-							<h1 class="counter">2536</h1>
-							<p>Projects Completed</p>
+							<h1 class="counter"><?php the_sub_field('number'); ?></h1>
+							<p><?php the_sub_field('number_text'); ?></p>
 						</div>
-						<div class="col-lg-3 col-md-6 single-fact">
-							<h1 class="counter">6784</h1>
-							<p>Happy Clients</p>
-						</div>
-						<div class="col-lg-3 col-md-6 single-fact">
-							<h1 class="counter">2239</h1>
-							<p>Cups of Coffee</p>
-						</div>	
-						<div class="col-lg-3 col-md-6 single-fact">
-							<h1 class="counter">435</h1>
-							<p>Real Professionals</p>
-						</div>												
+							
+					
+						<?php endwhile; ?>
+					
+					<?php endif; ?>
+					
+												
 					</div>
 				</div>	
 			</section>
